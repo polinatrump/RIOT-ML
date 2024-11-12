3509a4bb9536180797b26152a27f50e6040cf264
Zhaolan Huang <zhaolan94@gmail.com>
2024-11-12 19:32:26 +0100

    add cache machenism

---
+++ fusion_1dcached_poc.py
@@ -0,0 +1,115 @@
+import sys
+import os
+sys.path.append(os.getenv("TVM_HOME") + '/python')
+
+import tvm
+from tvm import relay
+from tvm.relay import dataflow_pattern as dfp
+from tvm.relay.transform import function_pass, InferType
+from tvm.micro import export_model_library_format
+import numpy as np
+
+from model_converter import compile_per_model_eval
+from mlmci_utils import generate_mlmci_files
+from partial_conv.relay_rewrite import rewrite_conv_chain_to_function
+
+# from tvm.relay.op.strategy.generic import conv2d_strategy
+
+# # conv2d_strategy.register("generic", conv2d_strategy.__wrapped__) # use unoptimized conv2d to avoid kernel_vec copy
+
+
+from partial_conv.dyn_slice_fixed_size import dyn_slice_fixed_size
+from partial_conv.op.copy_inplace import copy_in_place_spatial
+from partial_conv.utils import CollectOpShapeInfo, ReWriteInputsShape, ReWriteSwapVars, InferCallNodeType, EliminateIterateeDummyCallNode
+
+
+
+from partial_conv.op.fusion_iter_worker import fusion_iter_worker
+from partial_conv.op.strategy import conv2d
+
+from partial_conv.fusion_block_iteratee import create_fusion_block_iteratee_with_cache, create_zeros_for_vars
+
+if __name__=="__main__":
+    data = relay.var("data", shape=(1, 1, 224, 224))
+    # data = relay.var("data", shape=(1, 3, 15, 15))
+###################### Big ##############################333
+    weight1 = relay.var("weight1", shape=(64, 1, 7, 7))
+    weight2 = relay.var("weight2", shape=(64, 64, 3, 3))
+    weight3 = relay.var("weight3", shape=(64, 64, 3, 3))
+    dense_weight = relay.var("dense_weight", shape=(10, 64))
+
+    params = {"weight1": tvm.nd.array(np.random.rand(64, 1, 7, 7).astype(np.float32)),
+                "weight2": tvm.nd.array(np.random.rand(64, 64, 3, 3).astype(np.float32)),
+                "weight3": tvm.nd.array(np.random.rand(64, 64, 3, 3).astype(np.float32)),
+                "dense_weight": tvm.nd.array(np.random.rand(10, 64).astype(np.float32)),
+                }
+
+    conv1 = relay.nn.conv2d(data, weight1, kernel_size=(7, 7))
+    conv2 = relay.nn.conv2d(conv1, weight2, kernel_size=(3, 3))
+    conv3 = relay.nn.conv2d(conv2, weight3, kernel_size=(3, 3))
+    func = relay.Function([data, weight1, weight2, weight3], conv3)
+    mod = tvm.IRModule.from_expr(func)
+    mod = relay.transform.InferType()(mod)
+    iteratee_func, conv_chain_params, cache_vars, new_input_layout, new_input_stride, output_shape, input_shape = create_fusion_block_iteratee_with_cache(mod["main"].body, 1)
+
+    cache_zeros = create_zeros_for_vars(cache_vars)
+
+    in_w = input_shape[2]
+    in_h = input_shape[3]
+    iter_begin = [0,0,0,0]
+    iter_end = [0, 0, in_w - new_input_layout[0] + 1, in_h - new_input_layout[1] + 1]
+    iter_strides = [1,1, *new_input_stride]
+    # iterator = relay.zeros(shape=(4,), dtype="int32")
+    # iter_output_var = relay.zeros(shape=(1,64,15,15), dtype="float32")
+
+    # # dummy_var to keep the iteratee not to be optimized out
+    # dummy_var = relay.Call(iteratee_func, [iterator, data, weight1, weight2, weight3], tvm.ir.make_node("DictAttrs", iteratee_dummy=1))
+    # dummy_var = relay.zeros(shape=(4,), dtype="float32")
+    
+    iter_worker = fusion_iter_worker(iter_begin, iter_end, iter_strides, output_shape,[data, weight1, weight2, weight3, *cache_zeros], iteratee_func)
+    
+
+    iter_body = relay.Function(relay.analysis.free_vars(iter_worker), iter_worker)
+
+    # iter_body = EliminateIterateeDummyCallNode().visit(iter_body)
+
+    iteratee_mod = tvm.IRModule.from_expr(iter_body)
+    iteratee_mod['iteratee'] = iteratee_func
+    iteratee_mod = relay.transform.InferType()(iteratee_mod)
+
+    # breakpoint()
+    
+    RUNTIME = tvm.relay.backend.Runtime("crt", {'system-lib':False}) # should not use 'system-lib:true' while AoT
+    EXECUTOR = tvm.relay.backend.Executor(
+        "aot",
+        {
+        "unpacked-api": True, 
+        "interface-api": "c", 
+        "workspace-byte-alignment": 4,
+        "link-params": True,
+        },
+    )
+    TARGET = tvm.target.target.micro('host')
+    TARGET = "c -keys=partial_conv,arm_cpu,cpu -mcpu=cortex-m4+nodsp -model=nrf52840"
+
+    from tvm.ir.instrument import PrintAfterAll, PrintBeforeAll
+
+
+    with tvm.transform.PassContext(opt_level=0, config={
+                                                    "tir.disable_vectorize": True, 
+                                                    "tir.usmp.enable": True, # what is usmp? -> Enable Unified Static Memory Planning
+                                                    "tir.usmp.algorithm": "hill_climb",
+                                                    "relay.backend.use_auto_scheduler": True,
+                                                    "relay.remove_standalone_reshapes.enable": False
+                                                    },
+                                                    # instruments=[PrintBeforeAll(),PrintAfterAll()]
+                                                    ): 
+        # print(params.keys())
+        # opt_module, _ = relay.optimize(mod, target=TARGET)
+        module = relay.build(iteratee_mod, target=TARGET, runtime=RUNTIME, params=None, executor=EXECUTOR)
+
+    from tvm.micro import export_model_library_format
+
+    export_model_library_format(module, "./models/default/default.tar")
+    generate_mlmci_files(module, params, "./")
+
---
+++ partial_conv/op/cache_conv_input.py
@@ -0,0 +1,172 @@
+import sys
+import os
+sys.path.append(os.getenv("TVM_HOME") + '/python')
+
+import tvm
+from tvm import relay, topi
+import tvm.te
+
+from tvm.ir import Attrs
+import tvm.relay.op.op as _op
+import copy
+from tvm.target import generic_func, override_native_generic_func
+import math
+from tvm.ir import register_intrin_lowering, Op, register_op_attr
+
+
+# Define the new operator in Relay
+op_name = "cache_conv_input"
+relay.op.op.register(op_name)
+
+
+# call default relation functions
+def cache_conv_input_rel(args, attrs):
+    return args[1]
+_op.get(op_name).add_type_rel("CacheConvInputTypeRel", cache_conv_input_rel) # -> Key for TypeInference
+
+
+_op.get(op_name).set_support_level(1)
+_op.register_pattern(op_name, _op.OpPattern.ELEMWISE)
+_op.register_stateful(op_name, True)
+
+
+def cache_conv_input(data, buffer_shape, max_idx, conv_kernel_size, conv_strides, conv_padding, conv_dtype,layout="NCHW"):
+    attrs = tvm.ir.make_node("DictAttrs", buffer_shape=buffer_shape, max_idx=max_idx, 
+                             conv_kernel_size=conv_kernel_size, conv_strides=conv_strides, conv_padding=conv_padding,
+                             )
+    buffer_var = relay.var("cache_buffer_var", shape=buffer_shape, dtype=conv_dtype)
+    current_idx = relay.var("cache_cur_idx", shape=(4,), dtype="int32") # NCHW
+    return relay.Call(relay.op.get(op_name), [data, buffer_var, current_idx], attrs)
+
+
+def wrap_topi_schedule(topi_schedule):
+    """Wrap TOPI schedule which doesn't use attrs"""
+
+    def wrapper(attrs, outs, target):
+        with target:
+            return topi_schedule(outs)
+
+    return wrapper
+
+
+from tvm.topi import utils
+
+@relay.op.op.register_compute(op_name)
+def _compute(attrs, inputs, output_type):
+    print("We are now at cache_conv_input_compute")
+    
+    data, buffer_var, current_idx= inputs
+
+    # bg_x = utils.get_const_int(bg_x)
+    # bg_y = utils.get_const_int(bg_y)
+    max_idx = attrs["max_idx"]
+    buffer_shape = attrs["buffer_shape"]
+    kernel_size = attrs["conv_kernel_size"]
+    strides = attrs["conv_strides"]
+    def gen_ib(data_buf, buffer_var_buf, current_idx_buf, out_buf):
+        ib = tvm.tir.ir_builder.create()
+        data_w = data_buf.shape[3]
+        data = ib.buffer_ptr(data_buf)
+        buffer = ib.buffer_ptr(buffer_var_buf)
+        cur_idx = ib.buffer_ptr(current_idx_buf)
+        out = ib.buffer_ptr(out_buf)
+
+        #shrift elements inside buffer
+        with ib.for_range(0 , buffer_shape[0], "n") as n: # begin of for loop has to be zero for c codegen...
+            with ib.for_range(0,  buffer_shape[1], "i") as i:
+                with ib.for_range(0, buffer_shape[2], "j") as j:
+                    with ib.for_range(0, buffer_shape[3] - data_w, "k") as k:
+                        buffer[n , i , j , k] = buffer[n, i , j , k + data_w]
+
+        #Copy new data to buffer
+        with ib.for_range(0 , buffer_shape[0], "n") as n: # begin of for loop has to be zero for c codegen...
+            with ib.for_range(0,  buffer_shape[1], "i") as i:
+                with ib.for_range(0, buffer_shape[2], "j") as j:
+                    with ib.for_range(0, data_w, "k") as k:
+                        buffer[n , i , j , buffer_shape[3] - data_w + k] = data[n, i , j , k]
+
+        cur_idx[3] = cur_idx[3] + data_w
+
+        with ib.if_scope( tvm.tir.all((cur_idx[3] + 1) >= kernel_size[1], ((cur_idx[3] + 1 - kernel_size[1]) % strides[1]) == 0)):
+            # copy to Output buffer
+            with ib.for_range(0 , buffer_shape[0], "n") as n: # begin of for loop has to be zero for c codegen...
+                with ib.for_range(0,  buffer_shape[1], "i") as i:
+                    with ib.for_range(0, buffer_shape[2], "j") as j:
+                        with ib.for_range(0, buffer_shape[3], "k") as k:
+                            out[n , i , j , k] = buffer[n, i , j , k]
+
+        with ib.else_scope():
+            # skip output
+            ib.emit(tvm.tir.ret(-1))
+
+        return ib.get()
+    out_ib = [tvm.te.extern(output_type.shape, [data, buffer_var, current_idx],
+#                lambda ins, outs: gen_ib(ins[0], ins[1], ins[2]),
+              lambda ins, outs: gen_ib(ins[0], ins[1], ins[2], outs[0]),
+            name="cache_conv_input_compute.generic", 
+            # in_buffers=input_placeholders[1:],
+            # out_buffers=[input_placeholders[0]],
+            dtype=output_type.dtype,
+            # tag='inplace'
+            )]
+    return out_ib
+
+@override_native_generic_func(op_name + "_strategy")
+def _strategy(attrs, inputs, out_type, target):
+    strategy = _op.OpStrategy()
+    strategy.add_implementation(
+        _compute,
+        wrap_topi_schedule(topi.generic.schedule_extern),
+        name="iter_func.generic",
+    )
+    return strategy
+_op.register_strategy(op_name, _strategy)
+
+
+if __name__ == "__main__":
+    data = relay.var("data", shape=(1, 16, 7, 1), dtype="int32")
+
+    body = cache_conv_input(data, [1,32,7,3], [16,16], [3,3], [2,2], [0,0], "int32")
+
+    func = relay.Function(relay.analysis.free_vars(body), body)
+
+    mod = tvm.IRModule.from_expr(func)
+    # mod["iteratee"] = func
+    breakpoint()
+
+
+    mod = relay.transform.InferType()(mod)
+
+
+    # breakpoint()
+
+    RUNTIME = tvm.relay.backend.Runtime("crt", {'system-lib':False}) # should not use 'system-lib:true' while AoT
+    EXECUTOR = tvm.relay.backend.Executor(
+        "aot",
+        {
+        "unpacked-api": True, 
+        "interface-api": "c", 
+        "workspace-byte-alignment": 4,
+        "link-params": True,
+        },
+    )
+    TARGET = tvm.target.target.micro('host')
+
+    from tvm.ir.instrument import PrintAfterAll, PrintBeforeAll
+
+
+    with tvm.transform.PassContext(opt_level=0, config={
+                                                    "tir.disable_vectorize": True, 
+                                                    "tir.usmp.enable": False, # what is usmp? -> Enable Unified Static Memory Planning
+                                                    # "tir.usmp.algorithm": "hill_climb",
+                                                    "relay.backend.use_auto_scheduler": True,
+                                                    },
+                                                    # instruments=[PrintBeforeAll(),PrintAfterAll()]
+                                                    ): 
+        # print(params.keys())
+        # opt_module, _ = relay.optimize(mod, target=TARGET)
+        module = relay.build(mod, target=TARGET, runtime=RUNTIME, params=None, executor=EXECUTOR)
+        # breakpoint()
+    from tvm.micro import export_model_library_format
+
+    export_model_library_format(module, "./test_cache_conv_input.tar")
